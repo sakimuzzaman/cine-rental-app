ec734d544aee056ba6130394a1f0d045927e8006
sakimuzzaman <sakimuzzaman104@gmail.com>
2025-05-24 11:58:37 +0600

    using useState and react-context make this project

---
+++ cine-rental-app/src/context/index.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+
+const MovieContext = createContext();
+
+const ThemeContext = createContext();
+
+export {
+    MovieContext, ThemeContext
+};
---
+++ cine-rental-app/src/page.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import MovieList from "./components/MovieList"
+import Sidebar from "./components/Sidebar"
+import { ThemeContext } from "./context"
+
+export default function Page() {
+
+    const {darkMode} = useContext(ThemeContext);
+
+  return (
+    <div className={`h-full w-full ${darkMode ? "dark" : ""}`}>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
